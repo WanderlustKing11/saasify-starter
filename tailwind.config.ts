56f8c6a7ec385d98bb953977eb41040976166a3d
Wande <wanderlustking11@gmail.com>
2024-11-14 13:02:42 -0500

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
+export default config;
